f7f700f725af8df6f28f2636b6ee86840f20e7b3
BEMELON <hgdkim2@naver.com>
2022-11-26 03:37:25 +0900

    (#7) feat: get project_id by URL params

--- src/project/evaluationList/useEvaluationList.jsx
+++ src/project/evaluationList/useEvaluationList.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { getProjectPMEvaluation } from "../../common/api";
+import { useParams } from "react-router-dom";
 
 const useEvaluationList = () => {
+    const params = useParams();
+    console.log(params);
     const [evaluationList, setEvaluationList] = useState([]);
 
     useEffect(() => {
         async function fetch () {
-            const list = await getProjectPMEvaluation('박예연', 7);
+            const list = await getProjectPMEvaluation('박예연', params.project_id);
             setEvaluationList(list);
         }
         fetch();
