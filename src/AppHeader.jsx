835d06498839fa6c7f25334896ffdff32291fdf9
BEMELON <hgdkim2@naver.com>
2022-11-26 14:15:33 +0900

    (#16) feat: update app top-banner with MUI

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PrimarySearchAppBar from './AppTopBanner';
 
 const AppHeader = () => {
     return (
-        <ul>
-            <Link to="/login"><li>Login</li></Link>
-            <Link to="/project/search"><li>Project Search</li></Link>
-            <Link to="/project/7"><li>Project Info</li></Link>
-            <Link to="/project/7/evaluation"><li>Project Eval list</li></Link>
-            <Link to="/project/7/evaluation/pm"><li>Project PM Eval</li></Link>
-        </ul>
+        <><PrimarySearchAppBar /></>
     )
-
 }
 
 export default AppHeader;
