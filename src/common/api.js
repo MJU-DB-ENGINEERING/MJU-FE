1402d2782604b472859b045eb12be452cd78efb6
sungxsoo <njs783075@gmail.com>
2022-11-28 21:56:48 +0900

    fix: fixed id => using converting API

--- src/common/api.js
+++ src/common/api.js
@@ -1,88 +1,102 @@
-
 // const HOST_URL = 'http://13.125.105.3:50038/';
-const HOST_URL = '/'; // for CORS
+const HOST_URL = "/"; // for CORS
 
-import { fetch } from 'cross-fetch';
+import { fetch } from "cross-fetch";
 
 export const doLogin = async (userId, userPw) => {
-    const res = await fetch(HOST_URL + 'v1/login', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            "userId": userId,
-            "userPw": userPw,
-        }),
-    });
-    if (res.status == 200)
-        return res.json();
-    return undefined;
-}
+  const res = await fetch(HOST_URL + "v1/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: userId,
+      userPw: userPw,
+    }),
+  });
+  if (res.status == 200) return res.json();
+  return undefined;
+};
 
 export const searchProject = async (projectName) => {
-    const res = await fetch(HOST_URL + 'v1/search/project/' + projectName, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((response) => response.json());
+  const res = await fetch(HOST_URL + "v1/search/project/" + projectName, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
 
-    return res;
-}
+  return res;
+};
 
 export const getProjectDetail = async (projectName) => {
-    const res = await fetch(HOST_URL + 'v1/view/' + projectName + '/detail/', {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }).then((response) => response.json());
+  const res = await fetch(HOST_URL + "v1/view/" + projectName + "/detail/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
 
-    return res;
-}
+  return res;
+};
 
 export const updateProjectPMEvaluation = async (props) => {
-    const { businessComment, businessRate, 
-      communicationComment, communicationRate,
-      evaluated, evaluator,
-      projectId } = props;
+  const {
+    businessComment,
+    businessRate,
+    communicationComment,
+    communicationRate,
+    evaluated,
+    evaluator,
+    projectId,
+  } = props;
 
-    const res = await fetch(HOST_URL + 'v1/evaluation/pm/update', {
-        method: 'PUT',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            'businessComment': businessComment,
-            'businessRate': businessRate,
-            'communicationComment': communicationComment,
-            'communicationRate': communicationRate,
-            'evaluated': evaluated,
-            'evaluator': evaluator,
-            'projectId': projectId,
-        }),
-    }).then((response) => response.json());
+  console.log(evaluated);
+  const res = await fetch(HOST_URL + "v1/evaluation/pm/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      businessComment: businessComment,
+      businessRate: businessRate,
+      communicationComment: communicationComment,
+      communicationRate: communicationRate,
+      evaluated: evaluated,
+      evaluator: evaluator,
+      projectId: projectId,
+    }),
+  }).then((response) => response.json());
 
-    return res;
-}
+  return res;
+};
 
 export const getProjectPMEvaluation = async (evaluatorName, projectid) => {
-    const res = fetch(HOST_URL + 'v1/view/evalutaion', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            'evaluatorName': evaluatorName,
-            'projectId': projectid,
-        })
-    }).then((res) => res.json());
+  const res = fetch(HOST_URL + "v1/view/evalutaion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      evaluatorName: evaluatorName,
+      projectId: projectid,
+    }),
+  }).then((res) => res.json());
 
-    return (res);
-}
+  return res;
+};
 
 export const getCustomerName = (id) => {
-    if (id == 2)
-        return "LG CNS";
-}
+  if (id == 2) return "LG CNS";
+};
+
+export const getEmployeeID = async (name) => {
+  const res = await fetch(HOST_URL + "v1/convert/employee/name2id/" + name, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
+
+  return res;
+};
--- src/project/evaluation/EvaluationInputBox.jsx
+++ src/project/evaluation/EvaluationInputBox.jsx
@@ -1,104 +1,124 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import { updateProjectPMEvaluation } from "../../common/api";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { getEmployeeID, updateProjectPMEvaluation } from "../../common/api";
 import { useNavigate } from "react-router-dom";
 
 const EvaluationInputBox = (props) => {
-    const navigate = useNavigate();
-    const { evaluated, evaluator, projectId } = props.data;
-    const [businessComment, setBusinessComment] = useState('');
-    const [businessRating, setBusinessRating] = useState(0);
-    const [communicationComment, setCommunicationComment] = useState('');
-    const [communicationRating, setCommunicationRating] = useState(0);
+  const navigate = useNavigate();
+  const { evaluated, evaluator, projectId } = props.data;
+  const [businessComment, setBusinessComment] = useState("");
+  const [businessRating, setBusinessRating] = useState(0);
+  const [communicationComment, setCommunicationComment] = useState("");
+  const [communicationRating, setCommunicationRating] = useState(0);
 
-    const businessHander = (event) => {
-        setBusinessComment(event.target.value);
-    }
+  const businessHander = (event) => {
+    setBusinessComment(event.target.value);
+  };
 
-    const communicationHandler = (event) => {
-        setCommunicationComment(event.target.value);
-    }
+  const communicationHandler = (event) => {
+    setCommunicationComment(event.target.value);
+  };
 
-    const saveHandler = async () => {
-        const payload = {
-            "businessComment": businessComment,
-            "businessRate": businessRating,
-            "communicationComment": communicationComment,
-            "communicationRate": communicationRating,
-            "evaluated": 46, // fix-me
-            "evaluator": 64, // fix-me
-            "projectId": projectId
-        }
-        await updateProjectPMEvaluation(payload);
-        navigate(-1);
-    }
+  const saveHandler = async () => {
+    const id = await getEmployeeID(evaluated);
+    const payload = {
+      businessComment: businessComment,
+      businessRate: businessRating,
+      communicationComment: communicationComment,
+      communicationRate: communicationRating,
+      evaluated: id, // fix-me
+      evaluator: 64, // fix-me
+      projectId: projectId,
+    };
+    await updateProjectPMEvaluation(payload);
+    navigate(-1);
+  };
 
-    return (
-        <>
-        <Paper elevation={16} >
-            <Box sx={{ p: 2, border: '3px grey' }}>
-                <Typography variant="h4"> {evaluated} 님에 대한 프로젝트 평가를 시작합니다.</Typography>
-                <br />
-                <Typography variant="h6" width={200}>업무 수행 평가</Typography>
-                <Rating width={200}
-                    name="simple-controlled"
-                    size="large"
-                        value={businessRating}
-                    onChange={(event, newValue) => {
-                        setBusinessRating(newValue);
-                    } } 
-                />
-                <br />
-                <br />
-                <Typography variant="h6" width={200}>업무 수행 평가 내용</Typography>
-                <TextField width={200}
-                    id="outlined-multiline-flexible"
-                    label=""
-                    multiline
-                    minRows={3}
-                    maxRows={6}
-                    fullWidth
-                    value={businessComment}
-                    onChange={businessHander}
-                />
-                <br />
-                <br />
-                <Typography variant="h6" width={200}>커뮤니케이션 평가</Typography>
-                <Rating 
-                    name="simple-controlled"
-                    size="large"
-                    value={communicationRating}
-                    onChange={(event, newValue) => {
-                        setCommunicationRating(newValue);
-                    } } 
-                />
-                <br />
-                <br />
-                <Typography variant="h6" width={200}>커뮤니케이션 평가 내용</Typography>
-                <TextField 
-                    id="outlined-multiline-flexible"
-                    label=""
-                    multiline
-                    fullWidth
-                    minRows={3}
-                    maxRows={6}
-                    value={communicationComment}
-                    onChange={communicationHandler}
-                />
-                <Grid container justifyContent="flex-end" >
-                    <Button variant="outlined" sx={{ padding: 1, margin: 2 }} onClick={saveHandler}>저장하기</Button>
-                </Grid>
-            </Box>
-        </Paper>
-        </>
-    );
+  return (
+    <>
+      <Paper elevation={16}>
+        <Box sx={{ p: 2, border: "3px grey" }}>
+          <Typography variant="h4">
+            {" "}
+            {evaluated} 님에 대한 프로젝트 평가를 시작합니다.
+          </Typography>
+          <br />
+          <Typography variant="h6" width={200}>
+            업무 수행 평가
+          </Typography>
+          <Rating
+            width={200}
+            name="simple-controlled"
+            size="large"
+            value={businessRating}
+            onChange={(event, newValue) => {
+              setBusinessRating(newValue);
+            }}
+          />
+          <br />
+          <br />
+          <Typography variant="h6" width={200}>
+            업무 수행 평가 내용
+          </Typography>
+          <TextField
+            width={200}
+            id="outlined-multiline-flexible"
+            label=""
+            multiline
+            minRows={3}
+            maxRows={6}
+            fullWidth
+            value={businessComment}
+            onChange={businessHander}
+          />
+          <br />
+          <br />
+          <Typography variant="h6" width={200}>
+            커뮤니케이션 평가
+          </Typography>
+          <Rating
+            name="simple-controlled"
+            size="large"
+            value={communicationRating}
+            onChange={(event, newValue) => {
+              setCommunicationRating(newValue);
+            }}
+          />
+          <br />
+          <br />
+          <Typography variant="h6" width={200}>
+            커뮤니케이션 평가 내용
+          </Typography>
+          <TextField
+            id="outlined-multiline-flexible"
+            label=""
+            multiline
+            fullWidth
+            minRows={3}
+            maxRows={6}
+            value={communicationComment}
+            onChange={communicationHandler}
+          />
+          <Grid container justifyContent="flex-end">
+            <Button
+              variant="outlined"
+              sx={{ padding: 1, margin: 2 }}
+              onClick={saveHandler}
+            >
+              저장하기
+            </Button>
+          </Grid>
+        </Box>
+      </Paper>
+    </>
+  );
 };
 
 export default EvaluationInputBox;
